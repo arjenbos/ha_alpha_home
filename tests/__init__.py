ac0c1f22bbafc4355a64050c988fb586952e36a0
Arjen Bos <1064589+arjenbos@users.noreply.github.com>
2023-10-30 08:48:20 +0100

    Add tests and Github actions (#9)
    * Add tests for config flow and Base API

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+"""Tests for the Alpa Innotec integration."""
+
+MODULE = "custom_components.alpha_innotec"
+
+VALID_CONFIG = {
+    "gateway_ip": "127.0.0.1",
+    "gateway_password": "verysafe",
+    "controller_ip": "127.0.0.2",
+    "controller_username": "testing",
+    "controller_password": "alsoverysafe"
+}
